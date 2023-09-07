3393691ef645bd31b6bc75bbcf289a967a3f5a84
ananda dapa <dafaananda800@gmail.com>
2023-09-07 11:44:05 +0700

    add birthdays.db, imports

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,13 +1,12 @@
-from flask import Flask
-#import aplikasi flask untuk dipakai di web kita
+# import aplikasi flask, os, flash, jsonify, redirect, dan render_template untuk dipakai di web kita
+import os
 
-app = Flask(__name__)
-#mengatur nama aplikasi
-
-#mengatur uri (universal resource identifier), dan apa yang ditampilkan jika uri tersebut diakses
-@app.route('/') # ketika alamat http://127.0.0.1:5000/ dipanggil, maka server akan mengeksekusi fungsi hello()
-def hello(): # function dengan nama hello
-    return 'hello, world!'
+#import SQL utk akses database
+from cs50 import
+#import flash utk notifikasi pada web
+#import jsonify utk memformat data
+#import redirect dan render_template untuk berpindah halaman web
+   
 
 # mengatur URI ke http://127.0.0.1:5000/login, dan mengeksekusi fungsi login() jika diakses di alamat URI http://127.0.0.1:5000/login
 @app.route('/login')
